26aee8c39b00f63bd8dc393fccd275aeda0d7691
sprindjack <17216401+sprindjack@users.noreply.github.com>
2024-05-02 11:06:40 +0800

    Create 123.js

---
+++ 123.js
@@ -0,0 +1,23 @@
+[rewrite_local]
+
+^https:\/\/api\.goodnotes\.com\/accounts\/whoami\/(.*?) url script-response-body https://raw.githubusercontent.com/sprindjack/123/main/123.js
+
+[mitm]
+hostname = api.goodnotes.com
+
+
+
+var sj1010 = JSON.parse($response.body);
+
+sj1010["identity"]["metadata_public"]["metadata_public"]["entitlements"]["apple_access"] = {
+            "expires_at": 7943117641000
+          };
+sj1010["identity"]["metadata_public"]["metadata_public"]["entitlements"]["crossplatform_access"] = {
+            "expires_at": 7943117641000
+          };
+sj1010["identity"]["metadata_public"]["metadata_public"]["entitlements"]["premium"] = {
+            "expires_at": 7943117641000
+          };
+
+
+$done({body : JSON.stringify(sj1010)});
